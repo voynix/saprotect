7feb7ee415f5c21f7734e85c22515808b5c08ae2
Adam Jacobson <voynix26@gmail.com>
2016-08-07 14:44:01 -0400

    basically done

--- saprotect.py
+++ saprotect.py
@@ -1,18 +1,23 @@
+import argparse
 import sqlite3
 
 from hashlib import sha1
 from os import walk
-from os.path import abspath, join
-from sys import argv
-from time import time
+from os.path import abspath, basename, exists, isdir, join
+from time import asctime, localtime, time
 
 DATA_TABLE_NAME = 'files'
-SAPROTECT_TABLE_NAME = 'sapro'
 METADATA_TABLE_NAME = 'metadata'
+
+STATUS_OK = 0
+STATUS_MISMATCH = 1
+STATUS_REMEDIATE_NEW = 2
+STATUS_REMEDIATE_OLD = 3
+STATUS_NEW = 4
 
 CHUNK_SIZE = 4096
 
-VERSION = '0.1.0'
+VERSION = '1.0.0'
 HASH = 'SHA-1'
 
 class DB_Manager(object):
@@ -44,35 +49,38 @@ class DB_Manager(object):
     def create_tables(self):
         command = '''CREATE TABLE IF NOT EXISTS {}
                     (filename TEXT, path TEXT, hash TEXT, old_hash TEXT,
-                    time TEXT, old_time TEXT)'''.format(DATA_TABLE_NAME)
+                    time TEXT, old_time TEXT, status INTEGER)'''.format(DATA_TABLE_NAME)
         self.curs.execute(command)
         self.curs.execute('''CREATE TABLE IF NOT EXISTS {}
-                        (version TEXT, hash TEXT)'''.format(SAPROTECT_TABLE_NAME))
-        self.curs.execute('INSERT INTO {} VALUES (?, ?)'.format(SAPROTECT_TABLE_NAME), (VERSION, HASH))
-        self.curs.execute('''CREATE TABLE IF NOT EXISTS {}
-                        (start TEXT, end TEXT, files_scanned INTEGER)'''.format(METADATA_TABLE_NAME))
+                        (start TEXT, end TEXT, files_scanned INTEGER, files_added INTEGER,
+                         files_updated INTEGER, files_mismatched INTEGER)'''.format(METADATA_TABLE_NAME))
         self.conn.commit()
 
 
     def upsert_file(self, filename, path, hash):
-        command = 'SELECT COUNT(*) FROM {} WHERE filename = ? AND path = ?'.format(DATA_TABLE_NAME)
-        self.curs.execute(command, (filename, path))
-        count = self.curs.fetchone()[0]
-        if count == 0:
-            self.curs.execute('''INSERT INTO {}(filename, path, hash, time)
-                                 VALUES (?, ?, ?, ?)'''.format(DATA_TABLE_NAME),
-                              (filename, path, hash, time()))
-        else:
-            self.curs.execute('''UPDATE {} SET old_hash = hash, old_time = time
-                                 WHERE filename = ? AND path = ?'''.format(DATA_TABLE_NAME), (filename, path))
-            self.curs.execute('''UPDATE {} SET hash = ?, time = ?
-                                 WHERE filename = ? AND path = ?'''.format(DATA_TABLE_NAME),
-                              (hash, time(), filename, path))
+        command = 'SELECT * FROM {} WHERE path = ?'.format(DATA_TABLE_NAME)
+        self.curs.execute(command, (path,))
+        row = self.curs.fetchone()
+        if row is None:
+            self.curs.execute('''INSERT INTO {}(filename, path, hash, time, status)
+                                 VALUES (?, ?, ?, ?, ?)'''.format(DATA_TABLE_NAME),
+                              (filename, path, hash, time(), STATUS_NEW))
+        elif row[6] != STATUS_MISMATCH:  # note: only update if status is not mismatch
+            if row[6] == STATUS_REMEDIATE_OLD:
+                old_hash = row[3]
+                status = STATUS_OK if old_hash == hash else STATUS_MISMATCH
+                self.curs.execute('''UPDATE {} SET hash = ?, time = ?, status = ?
+                                     WHERE path = ?'''.format(DATA_TABLE_NAME), (hash, time, status, path))
+            else:
+                old_hash = row[2]
+                status = STATUS_OK if old_hash == hash else STATUS_MISMATCH
+                self.curs.execute('''UPDATE {} SET hash = ?, time = ?, old_hash = hash, old_time = time, status = ?
+                                     WHERE path = ?'''.format(DATA_TABLE_NAME), (hash, time(), status, path))
         self.conn.commit()
 
 
-    def check_files(self, show_hashes=False, clean=False):
-        self.curs.execute('SELECT * FROM {} WHERE NOT hash = old_hash'.format(DATA_TABLE_NAME))
+    def get_mismatches(self, show_hashes=False, clean=False):
+        self.curs.execute('SELECT * FROM {} WHERE status = {:d}'.format(DATA_TABLE_NAME, STATUS_MISMATCH))
         errors = 0
         if not clean:
             print 'MISMATCHES'
@@ -85,33 +93,63 @@ class DB_Manager(object):
                 print row[1]
         if not clean:
             print_sep()
-            if errors == 1:
-                print '1 mismatch found'
-            else:
-                print '{:d} mismatches found'.format(errors)
+            print '{:d} {} found'.format(errors, 'mismatch' if errors == 1 else 'mismatches')
         return errors
 
 
-    def get_statistics(self, clean=False):
-        self.curs.execute('SELECT COUNT(*) FROM {} WHERE old_hash IS NULL'.format(DATA_TABLE_NAME))
-        added = self.curs.fetchone()[0]
-        self.curs.execute('SELECT COUNT(*) FROM {} WHERE NOT old_hash IS NULL'.format(DATA_TABLE_NAME))
-        updated = self.curs.fetchone()[0]
-        self.curs.execute('SELECT COUNT(*) FROM {} WHERE NOT hash = old_hash'.format(DATA_TABLE_NAME))
-        mismatched = self.curs.fetchone()[0]
+    def get_info(self, clean=False):
+        self.curs.execute('SELECT * FROM {} ORDER BY start DESC'.format(METADATA_TABLE_NAME))
+        (start, end, scanned, added, updated, mismatched) = self.curs.fetchone()
         if not clean:
-            print 'STATISTICS'
+            print 'LAST SCAN {} {}'.format(asctime(localtime(float(start))), asctime(localtime(float(end))))
             print_sep()
-        print '{:d} files added'.format(added)
-        print '{:d} files updated'.format(updated)
-        print '{:d} files with hash mismatches'.format(mismatched)
+        print '{:d} file{} added'.format(added, 's' if added != 1 else '')
+        print '{:d} file{} updated'.format(updated, 's' if updated != 1 else '')
+        print '{:d} file{} with hash mismatches'.format(mismatched, 's' if mismatched != 1 else '')
         if not clean:
             print_sep()
-            print '{:d} files total'.format(added + updated)
+            print '{:d} total file{} scanned'.format(scanned, 's' if scanned != 1 else '')
 
-    def get_version(self):
-        self.curs.execute('SELECT * FROM {}'.format(SAPROTECT_TABLE_NAME))
-        print 'saprotect version {} using {}'.format(*self.curs.fetchone())
+    def remediate(self, path, status):
+        if exists(path):
+            if isdir(path):
+                for root, dirlist, filelist in walk(path, followlinks=True):
+                    for f in filelist:
+                        path = join(root, f)
+                        self.curs.execute('''SELECT status FROM {} WHERE path = ?'''.format(DATA_TABLE_NAME), (path,))
+                        if self.curs.fetchone()[0] == STATUS_MISMATCH:
+                            self.curs.execute('''UPDATE {} SET status = ? WHERE path = ?'''.format(DATA_TABLE_NAME),
+                                              (status, path))
+                            self.conn.commit()
+                        else:
+                            print '{} has no mismatch; skipping'.format(path)
+            else:
+                self.curs.execute('''SELECT status FROM {} WHERE path = ?'''.format(DATA_TABLE_NAME), (path,))
+                if self.curs.fetchone()[0] == STATUS_MISMATCH:
+                    self.curs.execute('''UPDATE {} SET status = ? WHERE path = ?'''.format(DATA_TABLE_NAME),
+                                      (status, path))
+                    self.conn.commit()
+                else:
+                    print '{} has no mismatch; skipping'.format(path)
+
+
+    def add_record(self, start_time, end_time, scanned):
+        self.curs.execute('SELECT COUNT(*) FROM {} WHERE status = {:d} AND time > {:f}'.format(DATA_TABLE_NAME,
+                                                                                               STATUS_NEW, start_time))
+        added = self.curs.fetchone()[0]
+        self.curs.execute('SELECT COUNT(*) FROM {} WHERE NOT status = {:d} AND time > {:f}'.format(DATA_TABLE_NAME,
+                                                                                                   STATUS_NEW, start_time))
+        updated = self.curs.fetchone()[0]
+        self.curs.execute('SELECT COUNT(*) FROM {} WHERE status = {:d}'.format(DATA_TABLE_NAME, STATUS_MISMATCH))
+        mismatched = self.curs.fetchone()[0]
+        self.curs.execute('''INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?)'''.format(METADATA_TABLE_NAME),
+                          (start_time, end_time, scanned, added, updated, mismatched))
+        self.conn.commit()
+
+
+    def get_num_mismatches(self):
+        self.curs.execute('''SELECT COUNT(*) FROM {} WHERE status = {:d}'''.format(DATA_TABLE_NAME, STATUS_MISMATCH))
+        return self.curs.fetchone()[0]
 
 
     def dump_database(self):
@@ -121,11 +159,9 @@ class DB_Manager(object):
 
 
 def protect_directory(directory, db):
-    # TODO: set onerror to something useful
     files_scanned = 0
     for root, dirlist, filelist in walk(directory, followlinks=True):  # follow symlinks
         for f in filelist:
-            # TODO: check for access first? (redundant with onerror above?)
             path = join(root, f)
             print 'hashing {}'.format(path)
             with open(path, 'r') as source:
@@ -141,19 +177,68 @@ def protect_directory(directory, db):
     return files_scanned
 
 
+def protect_file(path, db):
+    f = basename(path)
+    print 'hashing {}'. format(path)
+    with open(path, 'r') as source:
+        s = sha1()
+        chunk = source.read(CHUNK_SIZE)
+        while chunk != '':
+            s.update(chunk)
+            chunk = source.read(CHUNK_SIZE)
+        digest = s.hexdigest()
+        print 'storing {}'.format(path)
+        db.upsert_file(f, path, digest)
+
+
 def print_sep():
     print '-' * 40
 
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        print 'USAGE: {} PATH/TO/PROTECT/'.format(argv[0])
-        exit(1)
+    # TODO: for mismatches, find duplicates across folders?
+    parser = argparse.ArgumentParser(description='Record file hashes and check on changes')
+    parser.add_argument('-v', '--version', version='saprotect {} using {}'.format(VERSION, HASH), action='version',
+                        help='show version information')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-p', '--protect', metavar='TARGET', nargs='+',
+                        help='record the hashes of the TARGETs and print any mismatches found')
+    group.add_argument('-r', '--remediate-old', metavar='TARGET', nargs='+',
+                        help='resolve hash mismatches on the TARGETs in favor of the old hash')
+    group.add_argument('-R', '--remediate-new', metavar='TARGET', nargs='+',
+                        help='resolve hash mismatches on the TARGETs in favor of the new hash')
+    group.add_argument('-m', '--list-mismatches', action='store_true', help='show files with mismatched hashes')
+    arguments = parser.parse_args()
 
     with DB_Manager() as dbm:
-        directory = abspath(argv[1])
-        print str(protect_directory(directory, dbm)), 'files scanned'
-        dbm.dump_database()
-        dbm.check_files()
-        dbm.get_statistics()
-        dbm.get_version()
+        if arguments.protect is not None:
+            if dbm.get_num_mismatches() > 0:
+                print 'ERROR: Remediate mismatches below first!'
+                dbm.get_mismatches()
+                exit(0)
+            files_scanned = 0
+            start_time = time()
+            for target in arguments.protect:
+                target = abspath(target)
+                if exists(target):
+                    if isdir(target):
+                        files_scanned += protect_directory(target, dbm)
+                    else:
+                        protect_file(target, dbm)
+                        files_scanned += 1
+            end_time = time()
+            dbm.add_record(start_time, end_time, files_scanned)
+        elif arguments.remediate_old is not None:
+            for target in arguments.remediate_old:
+                target = abspath(target)
+                if exists(target):
+                    dbm.remediate(target, STATUS_REMEDIATE_OLD)
+        elif arguments.remediate_new is not None:
+            for target in arguments.remediate_new:
+                target = abspath(target)
+                if exists(target):
+                    dbm.remediate(target, STATUS_REMEDIATE_NEW)
+        elif arguments.list_mismatches:
+            dbm.get_mismatches(True)
+        else:
+            dbm.get_info()
